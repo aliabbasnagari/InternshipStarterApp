fb632d1f5dd16a3f53e5b4fb764b20589621e715
Ali Abbas <aliabbasinayatali@gmail.com>
2025-04-15 13:34:57 +0500

    Added transition from splash screen to main activity

--- app/src/main/java/com/cloudcare/internshipstarterapp/presentation/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/cloudcare/internshipstarterapp/presentation/activity/SplashScreenActivity.kt
@@ -1,21 +1,25 @@
 package com.cloudcare.internshipstarterapp.presentation.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.cloudcare.internshipstarterapp.MainActivity
 import com.cloudcare.internshipstarterapp.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+
     }
 }
